993137a123b86bf5ebb942d5587fd313448feae4
Michaelgwei86 <101484680+Michaelgwei86@users.noreply.github.com>
2022-11-17 19:23:17 +0100

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,17 @@
 resource "aws_instance" "web" {
     ami = var.AMI_ID
     instance_type = var.INSTANCE_TYPE
+    monitoring = true 
+    ebs_optimized = true
+    metadata_options {
+       http_endpoint = "enabled"
+       http_tokens   = "required"
+  }
+    
+    oot_block_device {
+        encrypted     = true
+    }
+    
     tags = {
         Name = "HelloWorld"
         }
